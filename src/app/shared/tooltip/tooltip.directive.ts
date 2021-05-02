2bc93e62fefc05d0cebb0740ae454570b8995e73
Kyle <wilichowskikyle@gmail.com>
2021-05-02 18:26:41 -0500

    chore(wip tooltip): tooltip

--- src/app/shared/tooltip/tooltip.directive.ts
+++ src/app/shared/tooltip/tooltip.directive.ts
@@ -1,68 +1,93 @@
 import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
+import { style, animate, keyframes } from '@angular/animations';
 
 @Directive({
   selector: '[appTooltip]'
 })
 export class TooltipDirective {
   @Input('appTooltip') tooltipStr: string;
-  tooltip: HTMLElement;
-  delay = 500;
+  @Input() position: 'top'|'right'|'bottom'|'left';
+  private tooltip: HTMLElement;
+  delay = 300;
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.showTooltip();
+    this.createTooltip();
+    this.renderer.appendChild(document.body, this.tooltip);
+    this.renderer.addClass(this.tooltip, 'tooltip');
+    setTimeout(() => {
+      this.handlePosition(this.position || 'right');
+      this.renderer.addClass(this.tooltip, 'fade-in');
+    }, this.delay)
   }
 
   @HostListener("mouseleave") onMouseLeave() {
-    this.renderer.setAttribute(this.tooltip, 'hidden', '');
-    // this.removeTooltip();
-     // on mouse over it will remove the opacity
+    this.renderer.addClass(this.tooltip, 'fade-out');
+    setTimeout(() => {
+      this.renderer.removeChild(document.body, this.tooltip);
+    }, this.delay);
   }
 
-  // showTooltip() {
-  //   this.tooltip = this.renderer.createElement('span');
-  //   this.tooltip.appendChild(this.renderer.createText(this.tooltipStr));
-  //   const parent = this.renderer.parentNode(this.el.nativeElement)
-  //   this.renderer.insertBefore(parent, this.tooltip, this.el.nativeElement);
-  // }
+  createTooltip() {
+    this.tooltip = this.renderer.createElement('div');
 
-  // removeTooltip() {
-  //   console.log('remove');
-  // }
-
-  showTooltip() {
-    this.tooltip = this.renderer.createElement('span');
-
-    const tooltipArrow = this.renderer.createElement('span');
-    this.renderer.addClass(tooltipArrow, 'tooltip-arrow');
-
-    const tooltipText = this.renderer.createElement('span');
-    this.renderer.addClass(tooltipText, 'tooltip-text');
-
+    const tooltipArrow = this.renderer.createElement('div');
+    this.renderer.addClass(tooltipArrow, `tooltip-arrow`);
     this.renderer.appendChild(this.tooltip, tooltipArrow);
+
+    const tooltipText = this.renderer.createElement('div');
+    this.renderer.addClass(tooltipText, 'tooltip-text');
     this.renderer.appendChild(this.tooltip, tooltipText);
 
     this.renderer.appendChild(
       tooltipText,
       this.renderer.createText(this.tooltipStr)
-      // adding the tooltip text into the tooltip span
     );
-    const { top, bottom, left, right, height, width } = this.el.nativeElement.getBoundingClientRect();
-    console.log(top, bottom, left, right, height, width)
 
-    const midHeight = bottom - (height / 2) - 15;
-    let ttTop, ttLeft;
+  }
 
-    ttTop = midHeight;
-    ttLeft = right;
-    this.renderer.setStyle(this.tooltip, 'top', `${ttTop}px`);
-    //adding a top positions value for the tooltip
-    this.renderer.setStyle(this.tooltip, 'left', `${ttLeft}px`);
-    // adding the left value
-    this.renderer.appendChild(document.body, this.tooltip);
-   // appending to the document
-    this.renderer.addClass(this.tooltip, 'tooltip');
-   // adding the tooltip styles
+  handlePosition(pos) {
+    console.log()
+    const ttWidth = this.tooltip.getBoundingClientRect().width;
+    const ttHeight = this.tooltip.getBoundingClientRect().height;
+    console.log(this.tooltip.getBoundingClientRect())
+    const { width, height, top, right, left, bottom } = this.el.nativeElement.getBoundingClientRect();
+    const midW = (width / 2) - (ttWidth / 2);
+    console.log(width, ttWidth)
+    const midH = (height / 2) - (ttHeight / 2);
+    let x, y;
+
+    const isOffScreen = (num) => num < 0 || num > window.innerWidth;
+
+    if (pos === 'top') {
+      x = top - ttHeight + 6;
+      y = midW;
+      if (isOffScreen(x)) {
+        x += ttHeight;
+        pos = 'bottom';
+      }
+    } else if (pos === 'right') {
+      x = top + midH;
+      y = right;
+    } else if (pos === 'bottom') {
+      x = bottom;
+      y = midW;
+    } else if (pos === 'left') {
+      x = top + midH;
+      y = left - ttWidth - 6;
+
+      if (isOffScreen(y)) {
+        y += ttWidth;
+        pos = 'right';
+      }
+    }
+
+    console.log('x', x, 'y', y);
+
+    this.renderer.setStyle(this.tooltip, 'top', `${x}px`);
+    this.renderer.setStyle(this.tooltip, 'left', `${y}px`);
+    this.renderer.addClass(this.tooltip, 'tooltip-' + pos);
+
   }
 }
