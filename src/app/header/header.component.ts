adb19ea13dd20ef07e4c7fba9835779d3ed928f3
Kyle <wilichowskikyle@gmail.com>
2021-05-02 18:41:03 -0500

    chore(dev branch): push changes to dev

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,26 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { DayService } from '../services/day.service';
-import { GenerationService } from '../services/generation.service';
-import { WeekDay } from '../services/day.interface';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  weekDays: any[];
-  selectedDay: WeekDay;
+export class HeaderComponent {
 
-  constructor(public readonly dayService: DayService) { }
+  constructor() { }
 
-  ngOnInit(): void {
-    this.weekDays = this.dayService.weekDays;
-  }
-
-  changeDay(day: WeekDay) {
-    if (this.dayService.selectedDay !== day) {
-      this.dayService.selectedDay = day;
-    }
-  }
 }
--- src/app/site-list/project-list.component.ts
+++ src/app/site-list/project-list.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { GenerationService } from '../services/generation.service';
 import { LoadingState, SelectItem } from '../services/generation.interface';
+import { DayService } from '../services/day.service';
+import { WeekDay } from '../services/day.interface';
 
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.css'],
 })
-export class ProjectListComponent {
+export class ProjectListComponent implements OnInit {
   isTableView: boolean = true;
+  weekDays: any[];
+  selectedDay: WeekDay;
   swepcoSource: string;
   displayCols: SelectItem[];
 
-  constructor(public genService: GenerationService) {
+  constructor(
+    public readonly genService: GenerationService,
+    public readonly dayService: DayService
+  ) {
     this.displayCols = [
       { label: 'Abbr', value: 'abbr' },
       { label: 'Name', value: 'name' },
@@ -25,6 +32,16 @@ export class ProjectListComponent {
   get loadingState(): LoadingState { return this.genService.loadingState; }
 
   get schedule() { return this.genService.schedule }
+
+  ngOnInit(): void {
+    this.weekDays = this.dayService.weekDays;
+  }
+
+  changeDay(day: WeekDay) {
+    if (this.dayService.selectedDay !== day) {
+      this.dayService.selectedDay = day;
+    }
+  }
 
   reloadWindow(): void {
     window.location.reload();
